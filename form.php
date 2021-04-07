42a0aa3c39a49f9ee2a5c09c45c3cacdb42f01f2
Vinayak <vinayaknayak4321@gmail.com>
2021-04-07 14:57:02 +0530

    Updated form.php

--- form.php
+++ form.php
@@ -4,13 +4,12 @@
     $visitoremail = $_POST['emailaddr'];
     $message = $_POST['message'];
     $emailsubject = "New Form submission";
-    $to = "vinayaknayak4321@gmail.com";
     $headers = "From : ". $visitoremail . "\r\n";
 
     $txt = "You have received an email from ".$name . "\n\n". $message;
     $txt = wordwrap($txt , 70);
 
-    mail($to, $emailsubject, $txt, $headers);
+    echo $txt;
 
     header("Location : index.php");
 
